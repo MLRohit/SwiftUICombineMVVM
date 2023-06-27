1520b5ff8c800ae3cee1d6b76b0acaf55f13e57f
Rohit  Lama <rohitlama1999@gmail.com>
2023-06-27 13:04:52 +0545

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Rohit Lama on 27/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
